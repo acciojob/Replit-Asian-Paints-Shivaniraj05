9ed87d9b3b41ab02bbaae5b69406e7de73e9ad66
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-02-02 01:40:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let colorForm = document.getElementById("form-action");
+
+colorForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let gridNo = document.getElementById("block_id").value;
+    let color = document.getElementById("colour_id").value;
+  
+
+    let block = document.getElementById(gridNo);
+
+    block.style.backgroundColor = color;
+
+});
+
+colorForm.addEventListener("click", () => {
+    let grids = document.querySelectorAll(".grid-item");
+    grids.forEach( (item)  => {
+        item.style.backgroundColor = 'transparent';
+    });
+});
